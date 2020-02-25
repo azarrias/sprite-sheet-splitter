58a42fd4ed1b34dd47bd6ce2b4374793280212e4
azarrias <adolfo.zarrias@gmail.com>
2020-02-25 19:53:04 +0100

    Scale the sprite sheet when the mouse wheel is moved and draw rectangles in order to preview the splitting

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,18 @@
 require 'globals'
 
---
--- LOVE callbacks
---
+local spriteSheetScale = 1
+local nrOfRows = 4
+local nrOfColumns = 4
+local spriteWidth = 16
+local spriteHeight = 32
+local xOffset = 0
+local yOffset = 0
+
 function love.load(arg)
+  if arg[#arg] == "-debug" then 
+    require("mobdebug").start() 
+  end
+  
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
   love.window.setTitle(GAME_TITLE)
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -19,12 +28,26 @@ function love.draw()
   love.graphics.setCanvas(canvas)
   love.graphics.setBlendMode('alpha', 'alphamultiply')
   love.graphics.draw(test, 0, 0)  
+  
+  love.graphics.setColor(0.9, 0.9, 0.9, 0.3)
+  love.graphics.setLineStyle('rough')
+  for y = 1, nrOfRows do
+    for x = 1, nrOfColumns do
+      love.graphics.rectangle('line', 
+        (x - 1) * spriteWidth + xOffset + 1, 
+        (y - 1) * spriteHeight + yOffset + 1, 
+        spriteWidth - 1, 
+        spriteHeight - 1)
+    end
+  end
+  love.graphics.setColor(1, 1, 1)
+  
   love.graphics.setCanvas()
   
   imgui.SetNextWindowPos(0, 0)
   imgui.SetNextWindowSize(love.graphics.getWidth(), love.graphics.getHeight())
   
-  if imgui.Begin("DockArea", nil, { "NoResize", "NoMove", "NoBrinToFrontOnFocus" }) then
+  if imgui.Begin("DockArea", nil, { "NoResize", "NoMove", "NoBringToFrontOnFocus" }) then
     imgui.BeginDockspace()
     
     imgui.SetNextDock("Right")
@@ -43,7 +66,7 @@ function love.draw()
     ]]
     imgui.SetNextDock("Left")
     imgui.BeginDock("Sprite sheet")
-    imgui.Image(canvas, test:getWidth() * 2, test:getHeight() * 2)
+    imgui.Image(canvas, test:getWidth() * spriteSheetScale, test:getHeight() * spriteSheetScale)
     imgui.EndDock()
    
     imgui.EndDockspace()
@@ -60,9 +83,6 @@ function love.quit()
   imgui.ShutDown();
 end
 
---
--- User inputs
---
 function love.textinput(t)
     imgui.TextInput(t)
     if not imgui.GetWantCaptureKeyboard() then
@@ -106,6 +126,7 @@ function love.mousereleased(x, y, button)
 end
 
 function love.wheelmoved(x, y)
+    spriteSheetScale = math.max(spriteSheetScale + y, 1)
     imgui.WheelMoved(y)
     if not imgui.GetWantCaptureMouse() then
         -- Pass event to the game
